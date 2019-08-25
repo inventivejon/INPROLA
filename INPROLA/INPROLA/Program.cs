a9c3a524ab67fbc0344511b535348ded118e3a65
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-08-25 21:02:42 +0200

    - Added NoSQL-Database in file simply based on example from https://github.com/mbdavid/LiteDB

--- INPROLA/INPROLA/Program.cs
+++ INPROLA/INPROLA/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Data.SQLite;
+using LiteDB;
 
 namespace INPROLA
 {
@@ -100,6 +101,49 @@ namespace INPROLA
             conn.Close();
         }
 
+        // Create your POCO class
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string[] Phones { get; set; }
+            public bool IsActive { get; set; }
+        }
+
+        static void NoSQLTest()
+        {
+            // Open database (or create if doesn't exist)
+            using (var db = new LiteDatabase(@"..\..\..\..\..\Base1.nosqlite"))
+            {
+                // Get customer collection
+                var col = db.GetCollection<Customer>("customers");
+
+                // Create your new customer instance
+                var customer = new Customer
+                {
+                    Name = "John Doe",
+                    Phones = new string[] { "8000-0000", "9000-0000" },
+                    Age = 39,
+                    IsActive = true
+                };
+
+                // Create unique index in Name field
+                col.EnsureIndex(x => x.Name, true);
+
+                // Insert new customer document (Id will be auto-incremented)
+                col.Insert(customer);
+
+                // Update a document inside a collection
+                customer.Name = "Joana Doe";
+
+                col.Update(customer);
+
+                // Use LINQ to query documents (with no index)
+                var results = col.Find(x => x.Age > 20);
+            }
+        }
+
         static void Main(string[] args)
         {
             #region READ INPUT FILE
@@ -119,6 +163,8 @@ namespace INPROLA
             //CreateTable(sqlite_conn);
             //InsertData(sqlite_conn);
             ReadData(sqlite_conn);
+            /* Perform small test with NoSQL Database */
+            NoSQLTest();
             #endregion
 
             #region PARSE INPUT AND BUILD MEANING TREE
