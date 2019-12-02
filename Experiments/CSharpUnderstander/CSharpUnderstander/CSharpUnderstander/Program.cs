8b1df76764f76dff2a6c69a8bfebdcc8d159fa5a
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-12-02 19:47:58 +0100

    - First steps for round trip INPROLA

---
+++ Experiments/CSharpUnderstander/CSharpUnderstander/CSharpUnderstander/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.CodeDom.Compiler;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace CSharpUnderstander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeProvider = new CSharpCodeProvider();
+
+            string filename = @"C:/Projects/INPROLA/Experiments/CSharpUnderstander/CodeExamples/CodeExample.cs";
+            
+            using (TextReader reader = File.OpenText(filename))
+            {
+                var parseResult = codeProvider.Parse(reader);
+
+                Console.Write(parseResult.Namespaces);
+            }
+        }
+    }
+}
---
+++ Experiments/CSharpUnderstander/CodeExamples/CodeExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.CodeDom.Compiler;
+using System.Text;
+using Microsoft.CSharp;
+
+namespace CSharpUnderstander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestEntry");
+        }
+    }
+}
